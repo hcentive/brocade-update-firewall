f09c0cb61c467b54498eed7311c7f2880c28810c
satyendra.sharma <satyendra.sharma@hcentive.com>
2016-07-08 07:36:04 -0400

    Commented out alientvault code from index.js

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 #!/usr/bin/env node
 
 var logger = require('./lib/logger.js');
-var av = require('./lib/alienvault.js');
+// var av = require('./lib/alienvault.js');
 var et = require('./lib/emergingthreats.js');
 var brocade = require('./lib/brocade.js');
 var tor = require('./lib/tor.js');
 
-var updateAVAddresses = function(callback) {
-  av.getAddresses(null, function(error, data) {
-    if (error) {
-      callback(error, null);
-    } else {
-      if (data.length > 0) {
-        brocade.updateBannedIPs(null, data, function(error, results) {
-          callback(error, results);
-        });
-      }
-    }
-  });
-};
+// var updateAVAddresses = function(callback) {
+//   av.getAddresses(null, function(error, data) {
+//     if (error) {
+//       callback(error, null);
+//     } else {
+//       if (data.length > 0) {
+//         brocade.updateBannedIPs(null, data, function(error, results) {
+//           callback(error, results);
+//         });
+//       }
+//     }
+//   });
+// };
 
 var updateETAddresses = function() {
   et.getAddresses(null, function(error, data) {
